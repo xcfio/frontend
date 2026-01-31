32a8b5405ac1c324c673be1cde47573259685238
xcfio <119097812+xcfio@users.noreply.github.com>
2026-01-31 14:52:20 +0600

    6th day (31-01-2026)

---
+++ 2026-01-31/password-generator/index.js
@@ -0,0 +1,31 @@
+import { letter } from "./letters"
+
+const btn = document.getElementById("btn")
+const p1 = document.getElementById("p1")
+const p2 = document.getElementById("p2")
+
+function characters(length) {
+    return Array(length)
+        .fill(0)
+        .map(() => Math.round(Math.random() * letter.length))
+        .map((x) => letter[x])
+        .join("")
+}
+
+btn.addEventListener("click", () => {
+    p1.style.backgroundColor = "#273549"
+    p2.style.backgroundColor = "#273549"
+    p1.textContent = characters(15)
+    p2.textContent = characters(15)
+})
+
+p1.addEventListener("click", () => {
+    navigator.clipboard.writeText(p1.textContent)
+    p1.style.backgroundColor = "#374151"
+})
+
+p2.addEventListener("click", () => {
+    navigator.clipboard.writeText(p2.textContent)
+    p2.style.backgroundColor = "#374151"
+})
+
