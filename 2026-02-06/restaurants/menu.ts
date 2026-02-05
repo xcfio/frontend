b737e039a2f8751cb18a1f4fdd85668936c58a8c
xcfio <119097812+xcfio@users.noreply.github.com>
2026-02-05 21:18:10 +0600

    Partial Restaurants project

---
+++ 2026-02-06/restaurants/menu.ts
@@ -0,0 +1,24 @@
+export const menu = [
+    {
+        name: "Pizza",
+        ingredients: ["pepperoni", "mushroom", "mozzarella"],
+        id: 0,
+        price: 14,
+        emoji: "🍕"
+    },
+    {
+        name: "Hamburger",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 12,
+        emoji: "🍔",
+        id: 1
+    },
+    {
+        name: "Beer",
+        ingredients: ["grain, hops, yeast, water"],
+        price: 12,
+        emoji: "🍺",
+        id: 2
+    }
+]
+
