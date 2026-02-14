b23f662d901b523e656febfccfd548b9c7f311f5
xcfio <119097812+xcfio@users.noreply.github.com>
2026-02-14 14:46:06 +0600

    fix??

--- 2026-02-14/digital-business-card/vite.config.ts
+++ 2026-02-14/digital-business-card/vite.config.ts
@@ -3,4 +3,4 @@ import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
-export default defineConfig({ base: "./", plugins: [react(), tailwindcss()] })
+export default defineConfig({ base: `${import.meta.dirname}/`, plugins: [react(), tailwindcss()] })
