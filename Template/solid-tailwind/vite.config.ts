77a51782652d6a3f8443f8b140819945cc2c9283
xcfio <119097812+xcfio@users.noreply.github.com>
2026-02-17 19:06:22 +0600

    Part two of https://github.com/xcfio/frontend/commit/386285b0c99ba85366eff61b82cd5efd21d0c985

--- Template/solid-tailwind/vite.config.ts
+++ Template/solid-tailwind/vite.config.ts
@@ -1,14 +1,9 @@
 import tailwindcss from "@tailwindcss/vite"
-import { defineConfig } from "vite"
 import solidPlugin from "vite-plugin-solid"
 import devtools from "solid-devtools/vite"
+import { defineConfig } from "vite"
 
 export default defineConfig({
     plugins: [devtools(), solidPlugin(), tailwindcss()],
-    server: {
-        port: 3000
-    },
-    build: {
-        target: "esnext"
-    }
+    build: { target: "esnext" }
 })
