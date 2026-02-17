cf331e42c80af2a48b26940bbba9313f4f393fd6
xcfio <119097812+xcfio@users.noreply.github.com>
2026-02-17 19:58:27 +0600

    nvm . .. ... .... .....

--- 2026-02-18/quizzical/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import tailwindcss from "@tailwindcss/vite"
-import solidPlugin from "vite-plugin-solid"
-import devtools from "solid-devtools/vite"
-import { defineConfig } from "vite"
-
-export default defineConfig({
-    plugins: [devtools(), solidPlugin(), tailwindcss()],
-    build: { target: "esnext" }
-})
